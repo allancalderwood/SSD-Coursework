71b0eafd7f84634e1f930850bf296c1db2435a4b
allancalderwood <ACALDE202@Caledonian.ac.uk>
2019-11-22 19:44:19 +0000

    added password reset view, html, css and js

--- SSD_Coursework/SSD_Coursework/urls.py
+++ SSD_Coursework/SSD_Coursework/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
    # path('', include('users.urls')),
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('admin/', admin.site.urls),
 ]
---
+++ SSD_Coursework/staticfiles/js/pass_reset.js
@@ -0,0 +1,6 @@
+var card = document.getElementById("login-container");
+card.classList.add("shake");
+
+var email = document.getElementsByTagName("input")[1];
+
+email.classList.add("errorShow");
