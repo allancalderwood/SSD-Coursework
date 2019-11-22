aadc0f4308a57794dc4f58d8d55dc3ee8512427f
allancalderwood <ACALDE202@Caledonian.ac.uk>
2019-11-22 17:21:55 +0000

    added login page template, css and image to project

--- SSD_Coursework/users/admin.py
+++ SSD_Coursework/users/admin.py
@@ -3,13 +3,13 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import Role
 
-# Register your models here.
 
 #  inline admin descriptor for role
 class RoleInLine(admin.StackedInline):
     model = Role
     can_delete = False
     verbose_name_plural = 'role'
+
 
 # new user admin
 class UserAdmin(BaseUserAdmin):
