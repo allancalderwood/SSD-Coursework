6b299b8559a3e4697d0fba6b83772b7207e75d3e
allancalderwood <ACALDE202@Caledonian.ac.uk>
2019-11-23 15:55:02 +0000

    added styling to password reset confirm

--- SSD_Coursework/staticfiles/js/reset_confirm.js
+++ SSD_Coursework/staticfiles/js/reset_confirm.js
@@ -5,3 +5,7 @@ var inputs = document.getElementsByTagName("input");
 inputs[1].placeholder = ('Enter a new password');
 
 inputs[2].placeholder = ('Confirm your new password');
+
+var errorList = document.getElementsByClassName('errorlist')[0];
+errorList.style.listStyleType = "none";
+errorList.style.color = "#ff584d";
