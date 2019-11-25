02ba6da6a5c1b99de662a93d0d766bf6f82f27b9
allancalderwood <ACALDE202@Caledonian.ac.uk>
2019-11-25 15:53:00 +0000

    test

--- SSD_Coursework/SSD_Coursework/urls.py
+++ SSD_Coursework/SSD_Coursework/urls.py
@@ -18,12 +18,14 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('', include('users.urls')),
     path('tickets/', include('tickets.urls')),
+    path('admin/', admin.site.urls),
+    #Logout
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    #Password Resets
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password_reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-    path('admin/', admin.site.urls),
 ]
