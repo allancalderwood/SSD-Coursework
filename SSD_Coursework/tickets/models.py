01d8ef4a67f1fb711c7f606f19865c1f919042fb
allancalderwood <ACALDE202@Caledonian.ac.uk>
2019-11-25 16:36:00 +0000

    Added Ticket and Comment models and added test records in the database

--- SSD_Coursework/tickets/models.py
+++ SSD_Coursework/tickets/models.py
@@ -1,3 +1,58 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Ticket(models.Model):
+
+    LOW = 'Low'
+    MED = 'Medium'
+    HIG = 'High'
+    PRIORITY = [
+        (LOW, 'Low'),
+        (MED, 'Medium'),
+        (HIG, 'High'),
+    ]
+
+    OPN = 'Open'
+    RES = 'Resolved'
+    CLS = 'Closed'
+    STATUS = [
+        (OPN, 'Open'),
+        (RES, 'Resolved'),
+        (CLS, 'Closed'),
+    ]
+
+    creatorID = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+
+    devID = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+
+    description = models.TextField()
+    priority = models.CharField(
+        max_length=7,
+        choices=PRIORITY
+    )
+
+    time = models.DateTimeField(auto_now=False, auto_now_add=True)
+    status = models.CharField(
+        max_length=8,
+        choices=STATUS
+    )
+
+
+class Comment(models.Model):
+    ticketID = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    content = models.TextField()
+    time = models.DateTimeField(auto_now=False, auto_now_add=True)
+    userID = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
